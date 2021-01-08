415b30db07627fef318a535683837f2f62b690ae
Ben Stone <benjamin.h.stone@gmail.com>
2021-01-08 10:57:10 -0500

    add sumNumbersAtEvenIndices

---
+++ problems/sumNumbersAtEvenIndices.js
@@ -0,0 +1,10 @@
+/** Sum all numbers at even indices in an array
+ * 
+* @param {number[]} arr - The input array
+* @returns {number} - The sum of all numbers at even indices
+*/
+function sumNumbersAtEvenIndices() {
+
+}
+
+module.exports = sumNumbersAtEvenIndices
