a06a80569f278c8b6c4afc300207803260f17dc9
Ben Stone <benjamin.h.stone@gmail.com>
2021-01-08 12:10:06 -0500

    add sortedClassNames

---
+++ problems/sortedClassNames.js
@@ -0,0 +1,13 @@
+/**
+* Returns an array of class names sorted in ascending lexigraphical order
+* 
+* @param {object[]} arr - The input array. Objects will be in the form: { subject: "math", studentCount: 10 }
+* @returns {string[]} - Returns an array of class names, sorted in ascending lexigraphical order
+* 
+*/
+
+function sortedClassNames() {
+
+}
+
+module.exports = sortedClassNames
