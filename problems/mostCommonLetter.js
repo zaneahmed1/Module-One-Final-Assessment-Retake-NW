1d64f30e1f079f05e9b238627466f13f478a603a
Ben Stone <benjamin.h.stone@gmail.com>
2021-01-08 11:56:35 -0500

    add mostCommonLetter

---
+++ problems/mostCommonLetter.js
@@ -0,0 +1,12 @@
+/**
+* Return the most common letter in a string
+*
+* @param {string} str - The input string
+* @returns {string} - Returns the most commonly occurring letter
+*/
+
+function mostCommonLetter() {
+  
+}
+
+module.exports = mostCommonLetter
