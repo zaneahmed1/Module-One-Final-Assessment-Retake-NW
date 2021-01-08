76680c345618effae1575db38ae3de508c10c504
Ben Stone <benjamin.h.stone@gmail.com>
2021-01-08 10:51:14 -0500

    add isMultipleOfFive

---
+++ problems/isMultipleOfFive.js
@@ -0,0 +1,11 @@
+/**
+* Determines whether or not a number is a multiple of five.
+* @param {number} n - The input number.
+* @returns {boolean} - True if n is a multiple of five, false if n is not a multiple of five, or not a number.
+*/
+
+function isMultipleOfFive() {
+
+}
+
+module.exports = isMultipleOfFive
