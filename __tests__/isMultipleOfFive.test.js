5285ee7c042c4478475203a006c3a9f0cd73622f
Ben Stone <benjamin.h.stone@gmail.com>
2021-01-08 13:03:31 -0500

    add tests for isMultipleOfFive

--- __tests__/isMultipleOfFive.test.js
+++ __tests__/isMultipleOfFive.test.js
@@ -1,5 +1,19 @@
 const countNumbers = require("../problems/isMultipleOfFive")
 
 describe("isMultipleOfFive", () => {
-
-}
+    test("Returns true for multiples of five", () => {
+        expect(isMultipleOfFive(5)).toBeTrue()
+        expect(isMultipleOfFive(10)).toBeTrue()
+        expect(isMultipleOfFive(140)).toBeTrue()
+        expect(isMultipleOfFive(125)).toBeTrue()
+        expect(isMultipleOfFive(1420)).toBeTrue()
+    })
+    test("Returns false for non-numbers of values that are not multiples of five", () => {
+        expect(isMultipleOfFive(1)).toBeFalse()
+        expect(isMultipleOfFive(19)).toBeFalse()
+        expect(isMultipleOfFive(3)).toBeFalse()
+        expect(isMultipleOfFive(13.5)).toBeFalse()
+        expect(isMultipleOfFive("Pancake")).toBeFalse()
+        expect(isMultipleOfFive("5Pancake10")).toBeFalse()
+    })
+})
