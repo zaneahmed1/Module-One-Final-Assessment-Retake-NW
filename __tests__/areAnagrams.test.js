87219e5e229ca81c6384be6d6b39828c9a13e4c7
Ben Stone <benjamin.h.stone@gmail.com>
2021-01-08 16:09:16 -0500

    remove digit tests

--- __tests__/areAnagrams.test.js
+++ __tests__/areAnagrams.test.js
@@ -7,11 +7,11 @@ describe("areAnagrams", () => {
         expect(areAnagrams("meteor", "remote")).toBe(true)
         expect(areAnagrams("night", "thing")).toBe(true)
     })
-    test("Returns false for non-strings of values that are not anagrams", () => {
+    test("Returns false for strings that are not anagrams", () => {
         expect(areAnagrams("dog", "cat")).toBe(false)
         expect(areAnagrams("apple", "leap")).toBe(false)
         expect(areAnagrams("pool", "poll")).toBe(false)
         expect(areAnagrams("aaab", "ba")).toBe(false)
-        expect(areAnagrams(4, 3)).toBe(false)
+        expect(areAnagrams("4", "3")).toBe(false)
     })
 })
