387cd15999960681da4fcc35fe16f41945e449ff
Ben Stone <benjamin.h.stone@gmail.com>
2021-01-08 12:23:57 -0500

    add multiplesOfThreeOrFive

---
+++ problems/multiplesOfThreeOrFive.js
@@ -0,0 +1,17 @@
+// Problem source: https://projecteuler.net/problem=1
+
+// If we list all the natural numbers below 10 that are multiples of 3 or 5, we get 3, 5, 6 and 9. The sum of these multiples is 23.
+
+// Find the sum of all the multiples of 3 or 5 below an input n.
+
+/**
+* Returns the sum of all the multiples of 3 or 5 below n
+* 
+* @param {number} n
+* @returns {number} - The sum of all the multiples of 3 or 5 that are less than n
+* 
+*/
+
+function multiplesOfThreeOrFive() {
+
+}
