31635715cc87a6d38c2446458f5a17944374491d
Ben Stone <benjamin.h.stone@gmail.com>
2021-01-08 11:58:49 -0500

    add totalNumberofStudents

---
+++ problems/totalNumberOfStudents.js
@@ -0,0 +1,13 @@
+/**
+* Returns the total number of students in all classes
+* 
+* @param {object[]} arr - The input array of classes. Objects will be in the form: { subject: "math", studentCount: 30 }
+* @returns {number} - The total number of students
+* 
+*/
+
+function totalNumberofStudents() {
+
+}
+
+module.exports = totalNumberofStudents
