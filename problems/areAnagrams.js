53d943b9fdd9703738d69e021f07d8dd9c978784
Ben Stone <benjamin.h.stone@gmail.com>
2021-01-08 11:54:45 -0500

    add areAnagrams

---
+++ problems/areAnagrams.js
@@ -0,0 +1,11 @@
+/** Returns whether two strings are anagrams.  Two strings are anagrams if they contain exactly the same letters in any order.
+ * 
+* @param {string} strOne - The first string
+* @param {string} strTwo - The second string
+* @returns {boolean} - True if the strings are anagrams, false otherwise
+*/
+function areAnagrams() {
+
+}
+
+module.exports = areAnagrams
