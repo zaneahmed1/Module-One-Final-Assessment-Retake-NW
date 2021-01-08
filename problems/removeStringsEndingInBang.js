57acdbc2e2c3c3dc867dfa5b143ded3a85ffe0d0
Ben Stone <benjamin.h.stone@gmail.com>
2021-01-08 10:55:26 -0500

    add removeStringsEndingInBang

---
+++ problems/removeStringsEndingInBang.js
@@ -0,0 +1,12 @@
+/**
+* Removes all strings ending in "!" from an array of strings.
+* 
+* @param {string[]} arr - The input array of strings
+* @returns {string[]} - Returns the strings in arr that don't end with "!"
+*/
+
+function removeStringsEndingInBang() {
+
+}
+
+module.exports = removeStringsEndingInBang
