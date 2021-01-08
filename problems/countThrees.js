e452c9565be420f6bff5c96d6bdf6f415d8c43a6
Ben Stone <benjamin.h.stone@gmail.com>
2021-01-08 10:52:49 -0500

    add countThrees

---
+++ problems/countThrees.js
@@ -0,0 +1,11 @@
+/**
+* Counts the number of threes in a given number.
+* @param {number} n - The input number.
+* @returns {number} - Returns how many threes are in the input.
+*/
+
+function countThrees() {
+
+}
+
+module.exports = countThrees
