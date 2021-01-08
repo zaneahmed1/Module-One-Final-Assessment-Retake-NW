30e97c03c7056ce45585a58a267b310a03c862a1
Ben Stone <benjamin.h.stone@gmail.com>
2021-01-08 11:40:28 -0500

    add sumEvenNumbers

---
+++ problems/sumEvenNumbers.js
@@ -0,0 +1,10 @@
+/** Sum all even numbers in an array
+ * 
+* @param {number[]} arr - The input array
+* @returns {number} - The sum of all even numbers
+*/
+function sumEvenNumbers() {
+
+}
+
+module.exports = sumEvenNumbers
