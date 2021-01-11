af07a14e71e0d74f85962950afe98448026f120c
Mike Boyle <mike@pursuit.org>
2021-01-11 15:01:19 -0500

    Rework `sortedClassNames` to avoid gotcha
    The original `sortedClassNames` would have required fellows to implement
    a solution in only one particular way, or else they would hit a gotcha
    involving JavaScript `Array.sort()` when sorting an array of objects
    by a string property. This will not work in the normal way and is not
    covered in the curriculum.
    
    To avoid this, I've reworked the problem to sort by class size instead of
    class name. I've also updated the test assertions and renamed the
    problem and test files accordingly.

---
+++ problems/sortedClassesBySize.js
@@ -0,0 +1,18 @@
+/**
+* Returns an array of class names sorted by number of students, from lowest to highest
+* 
+* @param {object[]} arr - The input array. Objects will be in the form: { subject: "math", studentCount: 10 }
+* @returns {string[]} - Returns an array of class names, sorted by class size in ascending order (lowest to highest)
+* 
+* ex: sortClassesBySize([ {subject: "math", studentCount: 50}, {subject: "gym", studentCount: 10}, {subject: "history", studentCount: 300}])
+* returns: ["gym", "math", "history"] (from fewest to most students)
+*
+* ex: sortClassesBySize([ {subject: "C", studentCount: 47}, {subject: "B", studentCount: 1}, {subject: "A", studentCount: 88}]) 
+* returns: ["B", "C", "A"]
+*/
+
+function sortClassesBySize() {
+
+}
+
+module.exports = sortClassesBySize
