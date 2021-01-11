7e8ff81a15d866f53ce58fee00e765f1e9bd1d57
Ben Stone <benjamin.h.stone@gmail.com>
2021-01-11 10:52:08 -0500

    change anagrams spec to letter counting spec

---
+++ __tests__/countLetters.test.js
@@ -0,0 +1,15 @@
+const areAnagrams = require("../problems/countLetters")
+
+describe("countLetters", () => {
+    test("Counts letters in a string with only letters", () => {
+        expect(countLetters("abcde")).toBe(5)
+        expect(countLetters("aaaAAA")).toBe(6)
+        expect(countLetters("")).toBe(0)
+        expect(countLetters("f")).toBe(1)
+    })
+    test("Counts letters and does not count characters that are not letters", () => {
+        expect(countLetters("Hi there")).toBe(7)
+        expect("A p . p ; l e s !").toBe(6)
+        expect(countLetters("....a..:()()")).toBe(1)
+    })
+})
